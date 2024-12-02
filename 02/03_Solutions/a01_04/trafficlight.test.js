9dfacbc6ccb281703fe5d61a2078c95a0e851482
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2024-12-03 00:18:50 +0100

    02 sol

---
+++ 02/03_Solutions/a01_04/trafficlight.test.js
@@ -0,0 +1,6 @@
+const app = require('./trafficlight');
+describe('test suite traffic light', () =>{
+    it('case red light', ()=>{
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    })
+})
