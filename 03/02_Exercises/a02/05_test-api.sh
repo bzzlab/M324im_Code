8f454fd56deb621a58f6dfbe5ff7bc03c35202e4
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2024-12-03 23:35:28 +0100

    02,03 demo+exer

---
+++ 03/02_Exercises/a02/05_test-api.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+# Attention:
+# Change CRLF (Windows) to LF (Unix on AWS) in your Editor
+#
+# Set variable PARAMS
+#??
+# check if PARAMS is set else exit
+#??
+#??
+#??
+#??
+# import PARAMS
+#??
+# Set variable MOD to node_modules
+#??
+# Check if node_modules exists
+#??
+    # Set variable file to package.json
+#??
+    # Check if package.json exists
+#??
+      # exist if not
+#??
+#??
+#??
+      # else install dependencies
+#??
+#??
+#??
+#??
+
+echo "test api of container $container"
+#??
---
+++ 03/02_Exercises/a03/00-Exercise.md
@@ -0,0 +1,9 @@
+### Exercise 3: Create a public repository 
+###  Prerequisite
+Your AWS EC2 instance is started with the private shared
+AMI ```bzz-ami```.
+### Tasks
+Create a public repository that contains
+1. Source-Code of the traffic light app
+2. Dockerfile for containerizing traffic light app
+3. All unit-tests for testing traffic light app over the REST-API
