f6deedc7c050ad514f51152fb281a35d6c89ccc4
dgaravaldi <daniel.garavaldi@gmail.com>
2025-11-11 16:21:37 +0100

    first commit

---
+++ 02/01_Intro/trafficlight.test.js
@@ -0,0 +1,8 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red"))
+            .toBe("DON'T WALK!");
+    });
+});
